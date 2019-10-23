eb8c572a185b236088fb602c15ec8054612f30df
ggilmore <geoffrey@sourcegraph.com>
2019-10-23 14:44:38 -0700

    add searcher cloudbuild.yaml

--- cmd/searcher/docker.sh
+++ cmd/searcher/docker.sh
@@ -3,7 +3,35 @@
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 set -euxo pipefail
 
-docker build -f cmd/searcher/Dockerfile -t $IMAGE . \
-    --build-arg COMMIT_SHA \
-    --build-arg DATE \
-    --build-arg VERSION
+BUILD_ARGS=(
+    "DATE"
+    "COMMIT_SHA"
+    "VERSION"
+)
+
+if [[ "$CI" == "true" ]]; then
+
+    substitutions="_IMAGE=$IMAGE"
+    for arg in "${BUILD_ARGS[@]}"; do
+        if [[ "${!arg}" ]]; then
+            substitutions+=",_${arg}=${!arg}"
+        fi
+    done
+
+    gcloud builds submit --config=cmd/searcher/cloudbuild.yaml \
+        --substitutions=$substitutions \
+        --no-source
+else
+
+    build_arg_str=""
+    for arg in "${BUILD_ARGS[@]}"; do
+        if [[ "${!arg}" ]]; then
+            build_arg_str+="--build-arg ${arg}=${!arg} "
+        fi
+    done
+
+    docker build -f cmd/searcher/Dockerfile -t "$IMAGE" . \
+        $build_arg_str \
+        --progress=plain
+
+fi
--- cmd/server/docker.sh
+++ cmd/server/docker.sh
@@ -24,7 +24,9 @@ if [[ "$CI" == "true" ]]; then
         fi
     done
 
-    gcloud builds submit --config=cmd/server/cloudbuild.yaml --substitutions=$substitutions --no-source
+    gcloud builds submit --config=cmd/server/cloudbuild.yaml \
+        --substitutions=$substitutions \
+        --no-source
 else
 
     build_arg_str=""
